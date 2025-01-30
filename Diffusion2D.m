64504d5c27628a45555e063689f1f5593ea6e44f
Adam Buchalter <abuchalt@vols.utk.edu>
2025-01-29 21:40:00 -0500

    skeleton framework for first solver

--- Diffusion2D.m
+++ Diffusion2D.m
@@ -7,7 +7,12 @@ j_max = 5;
 % Script
 % ------------------------------------------------------------------------------
 
+% Solving the Laplacian Heat Diffusion Equation: ∇⋅∇ T = 0
 
+% Discretize and convert to a linear system A*T = B
+
+% Solve the system via mldivide
+% T = A\B
 
 % Functions
 % ------------------------------------------------------------------------------
@@ -20,4 +25,3 @@ function [i, j] = revSeq(k, i_max)
     i = 1 + mod((k-1), i_max);
     j = 1 + ((k-i)/i_max);
 end
-
