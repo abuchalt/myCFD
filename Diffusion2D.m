5d0d9ef6182d32213ef88723dfc0aaf12e0831f8
Adam Buchalter <abuchalt@vols.utk.edu>
2025-01-30 10:36:20 -0500

    Completed the basic solver

--- Diffusion2D.m
+++ Diffusion2D.m
@@ -1,27 +1,100 @@
-% Computational Parameters
+%% Diffusion2D 
+% Author: Adam Buchalter
+% ------------------------------------------------------------------------------
+% This is a finite-difference solver for 2-dimensional, Laplacian heat diffusion
+% in cartesian coordinates using Dirichlet boundary conditions
+% ∇⋅∇ T = 0
+% ------------------------------------------------------------------------------
+clear all; close all; clc;
+
+%% Computational Parameters
 % ------------------------------------------------------------------------------
 
-i_max = 5;
-j_max = 5;
+% Define physical domain
+h = 1.0;
+w = 1.0;
 
-% Script
+% Define mesh size
+fprintf('Maximum number of points in x-direction') % separate print and input
+                                                   % b/c vscode extension
+i_max = input('');
+fprintf('Maximum number of points in y-direction')
+j_max = input('');
+
+% Define temperature at boundry
+T_boundry = 100.0;
+
+% Calculate step sizes
+Deltax = w/(i_max-1);
+Deltay = h/(j_max-1);
+
+%% Script
 % ------------------------------------------------------------------------------
 
-% Solving the Laplacian Heat Diffusion Equation: ∇⋅∇ T = 0
+% Discretize and convert to a linear system A*T = b
 
-% Discretize and convert to a linear system A*T = B
+% Init
+A = eye(i_max*j_max); % Coefficient Matrix
+b = zeros(i_max*j_max,1);
 
-% Solve the system via mldivide
-% T = A\B
+% Define Coefficient Matrix
+for i = 2:i_max-1
+    for j = 2:j_max-1
+        k = pmap(i, j, i_max);
+        k_e = k + 1;
+        k_w = k - 1;
+        k_n = k + i_max;
+        k_s = k - i_max;
 
-% Functions
+        % pointer mapping goes row-by-row to assemble Coeff. Matrix
+        A(k,k) = -2.0/Deltax^2 - 2.0/Deltay^2;
+        A(k,k_e) = 1.0/Deltax^2;
+        A(k,k_w) = 1.0/Deltax^2;
+        A(k,k_n) = 1.0/Deltay^2;
+        A(k,k_s) = 1.0/Deltay^2;
+    end
+end
+
+% Apply BCs
+for i = 1:1
+    for j = 1:j_max
+        k = pmap(i,j,i_max);
+        b(k,1) = T_boundry;
+    end
+end
+
+% Solve the linear system via mldivide
+T = A\b;
+
+%% Output Results
 % ------------------------------------------------------------------------------
 
-function k = seq(i, j, i_max)
+% Init
+
+% Define x and y values
+for i = 1:i_max
+    for j = 1:j_max
+        x(i,j) = Deltax*(i-1);
+        y(i,j) = Deltay*(j-1);
+        % k = pmap(i,j,i_max); % Alternative to reshape (below)
+        % T_plot(i,j) = T(k,1);
+    end
+end
+
+% Reshape Solution T to imax/jmax mesh
+T_plot = reshape(T,i_max,j_max);
+
+% Produce isotherm level contour plot of data
+contour(x,y,T_plot)
+
+%% Functions
+% ------------------------------------------------------------------------------
+
+function k = pmap(i, j, i_max)
     k = i + (j-1)*i_max;
 end
 
-function [i, j] = revSeq(k, i_max)
+function [i, j] = revPmap(k, i_max)
     i = 1 + mod((k-1), i_max);
     j = 1 + ((k-i)/i_max);
 end
