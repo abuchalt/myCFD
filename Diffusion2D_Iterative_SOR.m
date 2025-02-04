1a4ce9ca843dfa22bb9488d47ba9e8ce00f144d3
Adam Buchalter <abuchalt@vols.utk.edu>
2025-02-04 10:31:22 -0500

    refined nomenclature to capture pseudo-timestepping

--- Diffusion2D_Iterative_SOR.m
+++ Diffusion2D_Iterative_SOR.m
@@ -27,47 +27,12 @@ T_boundry = 100.0;
 residual = 1.0E5;
 epsilon = 1.0E-9;
 
-% Define relaxation factor
-alpha = 1.8;
+% Define relaxation factor (pseudo-timestepping)
+Deltatau = 1.8;
 
 % Calculate step sizes
 Deltax = w/(i_max-1);
 Deltay = h/(j_max-1);
-
-%% Script
-% ------------------------------------------------------------------------------
-
-% Discretize and convert to a linear system A*T = b
-
-% Init
-T = zeros(i_max,j_max)
-% Apply BCs
-T(1,:) = T_boundry
-
-% Define Coefficient Matrix
-iter = 1;
-T_old = T;
-tstart = tic; % Measure computation time
-while (residual > epsilon)  
-    for i = 2:i_max-1
-        for j = 2:j_max-1
-            % T(i,j) = ((T(i+1,j)+T(i-1,j))/Deltax^2 + (T(i,j+1)+T(i,j-1))/Deltay^2)/(2.0/Deltax^2 + 2.0/Deltay^2); % Unrelaxed Gauss-Seidal
-            % T(i,j) = T(i,j) + ( (T(i+1,j)-2*T(i,j)+T(i-1,j))/Deltax^2 + (T(i,j+1)-2*T(i,j)+T(i,j-1))/Deltay^2 ) / (2.0/Deltax^2 + 2.0/Deltay^2); % Relaxation form w/ same performance (no over-relaxation factor)
-            % overrelaxation: α > 1.0   --Increases rate of convergence, too large will cause unstable
-            T(i,j) = T(i,j) + alpha*( (T(i+1,j)-2*T(i,j)+T(i-1,j))/Deltax^2 + (T(i,j+1)-2*T(i,j)+T(i,j-1))/Deltay^2 ) / (2.0/Deltax^2 + 2.0/Deltay^2);
-        end
-    end
-    iter = iter + 1;
-    residual = norm(T-T_old);
-    fprintf(1,'iter = %i, residual = %g\n',iter,log10(residual))
-    T_old = T;
-end
-toc(tstart);
-
-%% Output Results
-% ------------------------------------------------------------------------------
-
-% Init
 
 % Define x and y values
 for i = 1:i_max
@@ -78,6 +43,42 @@ for i = 1:i_max
         % T_plot(i,j) = T(k,1);
     end
 end
+
+%% Script
+% ------------------------------------------------------------------------------
+
+% Discretize and convert to a linear system A*T = b
+
+% Init
+T = zeros(i_max,j_max);
+% Apply BCs
+T(1,:) = T_boundry;
+
+% Define Coefficient Matrix
+iter = 1;
+T_old = T;
+tstart = tic; % Measure computation time
+while (residual > epsilon)  
+    for i = 2:i_max-1
+        for j = 2:j_max-1
+            % T(i,j) = ((T(i+1,j)+T(i-1,j))/Deltax^2 + (T(i,j+1)+T(i,j-1))/Deltay^2)/(2.0/Deltax^2 + 2.0/Deltay^2); % Unrelaxed Gauss-Seidal
+            % T(i,j) = T(i,j) + ( (T(i+1,j)-2*T(i,j)+T(i-1,j))/Deltax^2 + (T(i,j+1)-2*T(i,j)+T(i,j-1))/Deltay^2 ) / (2.0/Deltax^2 + 2.0/Deltay^2); % Relaxation form w/ same performance (no over-relaxation factor)
+            % overrelaxation: Δτ > 1.0   --Increases rate of convergence, too large will diverge
+            T(i,j) = T(i,j) + Deltatau*( (T(i+1,j)-2*T(i,j)+T(i-1,j))/Deltax^2 + (T(i,j+1)-2*T(i,j)+T(i,j-1))/Deltay^2 ) / (2.0/Deltax^2 + 2.0/Deltay^2);
+        end
+    end
+    iter = iter + 1;
+    residual = norm(T-T_old);
+    fprintf(1,'iter = %i, residual = %g\n',iter,log10(residual))
+    T_old = T;
+    % draw pseudo-time evolution live
+    % contour(x,y,T);
+    % drawnow;
+end
+toc(tstart);
+
+%% Output Results
+% ------------------------------------------------------------------------------
 
 % Produce isotherm level contour plot of data
 contour(x,y,T)
